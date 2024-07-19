473f316fc2aafe79f3ce1381be290e2538b084c1
Nadezhda <temurazova.n@gmail.com>
2024-07-19 16:01:52 +0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <ctime>
+#include <fstream>
+
+int main() {
+    std::ofstream file("../binaryPic.txt", std::ios::binary);
+    if (!file) {
+        return 0;
+    }
+    std::srand(std::time(nullptr));
+    int height;
+    int width;
+    std::cout << "Enter height and width: ";
+    std::cin >> height >> width;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+           file << std::rand() % 2;
+        }
+        file << std::endl;
+    }
+
+    file.close();
+
+    return 0;
+}
